6e9b15325a2352322c68bd8fb077525d51f4a5d0
QUANGTRAN <dinhquang251@gmail.com>
2018-10-22 15:50:44 +0700

    create_ui

---
+++ app/src/main/java/com/example/quangtran/listenmusic/Model/SongModel.java
@@ -0,0 +1,13 @@
+package com.example.quangtran.listenmusic.Model;
+
+public class SongModel  {
+    public String getPath() {
+        return path;
+    }
+
+    public void setPath(String path) {
+        this.path = path;
+    }
+
+    public String path;
+}
---
+++ app/src/main/java/com/example/quangtran/listenmusic/View/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.quangtran.listenmusic.View;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.quangtran.listenmusic.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
